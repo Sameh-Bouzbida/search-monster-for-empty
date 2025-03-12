8a33f0734e62dd121cd1d3af95fccb50348033ae
samehb <bouzbidasameh@gmail.com>
2025-03-12 21:34:59 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, computed, model} from '@angular/core';
 import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 import { Monster } from './models/monster.model';
-
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { MonsterType } from './utils/monster.utils';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true, // Indique que ce composant est autonome (standalone)
-  imports: [PlayingCardComponent], // Importe les composants enfants nécessaires
+  imports: [CommonModule, PlayingCardComponent, SearchBarComponent], // Importe les composants enfants nécessaires
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   
-  monsters!: Monster[]; // Tableau contenant la liste des monstres
-  
+  monsters: Monster[] ;
+  search = model("");
 
-  selectedMonsterIndex = 0; // Index du monstre actuellement affiché
-
+  filteredMonsters = computed(()=> {
+    return this.monsters.filter(monster => monster.name.includes(this.search()));
+  })
+ 
   constructor() {
-    this.monsters = []; // Initialisation du tableau des monstres
+    
+    this.monsters = [];
 
-    // Création du premier monstre
+    // Création et ajout des monstres à la liste
     const monster1 = new Monster(); 
     monster1.name = "Pik";
     monster1.hp = 40;
-    monster1.figureCaption = "N°002 Pik";
-    this.monsters.push(monster1); // Ajout du monstre à la liste
+    monster1.figureCaption = "N°001 Pik";
+    this.monsters.push(monster1); // Ajout du premier monstre
 
-    // Création du second monstre
+    // Création et ajout des monstres à la liste
     const monster2 = new Monster(); 
     monster2.name = "Car";
     monster2.image = "assets/img/car.PNG";
     monster2.type = MonsterType.WATER;
     monster2.hp = 60;
-    monster2.figureCaption = "N°003 Car";
-    this.monsters.push(monster2); // Ajout du monstre à la liste
+    monster2.figureCaption = "N°002 Car";
+    this.monsters.push(monster2); // Ajout du second monstre
+
+    // Création et ajout des monstres à la liste
+    const monster3 = new Monster(); 
+    monster3.name = "Bulb";
+    monster3.image = "assets/img/bulb.PNG";
+    monster3.type = MonsterType.PLANT;
+    monster3.hp = 40;
+    monster3.figureCaption = "N°003 Bulb";
+    this.monsters.push(monster3); // Ajout du premier monstre
+
+    // Création et ajout des monstres à la liste
+    const monster4 = new Monster(); 
+    monster4.name = "Sala";
+    monster4.image = "assets/img/sala.PNG";
+    monster4.type = MonsterType.FIRE;
+    monster4.hp = 60;
+    monster4.figureCaption = "N°004 Sala";
+    this.monsters.push(monster4); // Ajout du second monstre
+ 
   }
 
   
-
-  // Change le monstre affiché en passant au suivant dans la liste
-  toggleMonster() {
-    this.selectedMonsterIndex = (this.selectedMonsterIndex + 1) % this.monsters.length;
-  }
 }
--- src/app/components/playing-card/playing-card.component.ts
+++ src/app/components/playing-card/playing-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component,  computed, input } from '@angular/core';
 import { Monster } from '../../models/monster.model';
 import { MonsterTypeProperties } from '../../utils/monster.utils';
 
@@ -8,29 +8,21 @@ import { MonsterTypeProperties } from '../../utils/monster.utils';
   templateUrl: './playing-card.component.html',
   styleUrl: './playing-card.component.css'
 })
-export class PlayingCardComponent implements OnChanges {
+export class PlayingCardComponent  {
   
   // Propriété d'entrée permettant de recevoir un monstre depuis un composant parent
-  @Input() monster = new Monster();
+  monster = input(new Monster());
   
   // Icône représentant le type du monstre (par défaut, celui du type électrique)
-  monsterTypeIcon: string = "assets/img/electric.PNG";
+  monsterTypeIcon= computed (() => {
+    return MonsterTypeProperties[this.monster().type].imageUrl;
+  }
+);
   
   // Couleur de fond associée au type du monstre (par défaut, celle du type électrique)
-  backgroundColor: string = "rgb(255, 255, 104)";
+  backgroundColor = computed(() => {
+    return MonsterTypeProperties[this.monster().type].color;
+  });
 
-  /**
-   * Détecte les changements sur la propriété d'entrée `monster`.
-   * Si le type du monstre change, met à jour l'icône et la couleur de fond.
-   */
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['monster']) { // Vérifie si un changement a eu lieu sur la variable `monster`
-      // Vérifie si le type du monstre a changé par rapport à l'ancienne valeur
-      if (changes['monster'].previousValue?.type != changes['monster'].currentValue.type) {
-        // Met à jour l'icône et la couleur de fond en fonction du nouveau type du monstre
-        this.monsterTypeIcon = MonsterTypeProperties[this.monster.type].imageUrl;
-        this.backgroundColor = MonsterTypeProperties[this.monster.type].color;
-      }
-    }
-  }
+  
 }
